bac7ed939c48fbbbfce50fa43ef23380c15f1e30
Kateryna Sukovaia <122564463+Katerynas82@users.noreply.github.com>
2024-09-26 21:41:46 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+import { fetchImages } from "./Components/api";
+import ImagesGallery from "./Components/ImagesGallery/ImagesGallery";
+import Loader from "./Components/Loader/Loader";
+import ErrorMessage from "./Components/ErrorMessage/ErrorMessage";
+import LoadMoreBtn from "./Components/LoadMoreBtn/LoadMoreBtn";
+import SearchBar from "./Components/SearchBar/SearchBar";
+import ImageModal from "./Components/ImageModal/ImageModal";
+
+const App = () => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [imageModal, setImageModal] = useState(false);
+  const [largeImageUrl, setLargeImageUrl] = useState("");
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const getImages = async () => {
+      try {
+        setIsError(false);
+        setIsLoading(true);
+        const data = await fetchImages(page, query);
+        setIsLoading(false);
+        setImages((prev) => [...prev, ...data]);
+      } catch {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    if (query) {
+      getImages();
+    }
+  }, [page, query]);
+
+  const handleChangePage = () => {
+    setPage((prev) => prev + 1);
+  };
+
+  const handleSetQuery = (topic) => {
+    setQuery(topic);
+    setImages([]);
+    setPage(1);
+  };
+  const handleImageClick = (url) => {
+    setLargeImageUrl(url);
+    setImageModal(true);
+  };
+
+  return (
+    <div>
+      <SearchBar setQuery={handleSetQuery} />
+      {images.length > 0 && (
+        <ImagesGallery images={images} handleImageClick={handleImageClick} />
+      )}
+      {images.length > 0 && !isLoading && (
+        <LoadMoreBtn handleChangePage={handleChangePage} />
+      )}
+
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+
+      <ImageModal
+        imageModal={imageModal}
+        setImageModal={setImageModal}
+        largeImageUrl={largeImageUrl}
+      />
+    </div>
+  );
+};
+
+export default App;
